48bc96dac19136dcf558e104e4582d9c4191f1a2
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2018-03-21 17:16:17 -0500

    Add myAccount Page
    Fixed navbar size issues

--- src/main/java/com/alaythiaproductions/bookstore/controllers/HomeController.java
+++ src/main/java/com/alaythiaproductions/bookstore/controllers/HomeController.java
@@ -2,15 +2,22 @@ package com.alaythiaproductions.bookstore.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/")
     public String index(Model model) {
         model.addAttribute("title", "Bookstore Home");
         return "views/index";
+    }
+
+    @GetMapping(value = "/myAccount")
+    public String myAccount(Model model) {
+        model.addAttribute("title", "My Account");
+        return "views/myAccount";
     }
 }
---
+++ src/main/java/com/alaythiaproductions/bookstore/models/User.java
@@ -0,0 +1,15 @@
+package com.alaythiaproductions.bookstore.models;
+
+public class User {
+
+    private long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String phone;
+    private boolean enabled = true;
+
+}
